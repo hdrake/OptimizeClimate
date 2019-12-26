a7c4109a87075c9f7e421fd8280b80fdbed5ad36
Henri Drake <henrifdrake@gmail.com>
2019-12-26 18:01:41 -0500

    Calculated uncertainty in SCC due to uncertain ECS

--- src/ensemble.jl
+++ src/ensemble.jl
@@ -1,10 +1,12 @@
-function ensemble_diagnostic_statistics(ensemble::Dict{String, ClimateModel}, diagnostic::Function, domain::Array{Float64,1})
+function ensemble_diagnostic_statistics(ensemble::Dict{String, ClimateModel}, diagnostic::Function, domain::Array{Float64,1}, args...)
     
     diag_arr = zeros(length(ensemble), length(domain))
     
+    print(args)
+    
     count_model = 1
     for (name, model) in ensemble
-        diag_arr[count_model,:] = diagnostic(model)
+        diag_arr[count_model,:] = diagnostic(model, args...)
         count_model+=1
     end
     
@@ -13,6 +15,23 @@ function ensemble_diagnostic_statistics(ensemble::Dict{String, ClimateModel}, di
     stats = hcat(map(myquantile, (diag_arr[:,i] for i=1:size(diag_arr,2)))...)
     
     return stats[1,:], stats[2,:], stats[3,:]
+end
+
+function ensemble_diagnostic_statistics_scalar(ensemble::Dict{String, ClimateModel}, diagnostic::Function, domain::Array{Float64,1}, args...)
+    
+    diag_arr = zeros(length(ensemble))
+    
+    count_model = 1
+    for (name, model) in ensemble
+        diag_arr[count_model] = diagnostic(model, args...)
+        count_model+=1
+    end
+    
+    myquantile(arr) = quantile(arr, [0.1, 0.5, 0.9])
+    
+    stats = myquantile(diag_arr)
+    
+    return stats[1], stats[2], stats[3]
 end
 
 function ensemble_state_statistics(ensemble::Dict{String, ClimateModel}, symbols::Array{Symbol,1}, domain::Array{Float64,1})
